24bd71e36dfb0451c2ecd8ec2193ae78f007409a
Omar del Real <zelios@hotmail.es>
2021-04-29 21:13:14 +0200

    feature(add-routing): Added Reach Router library and some basic home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { PageProps } from '../types'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const HomePage = (props: PageProps): JSX.Element => {
+  return <h1>Home</h1>
+}
+
+export default HomePage
